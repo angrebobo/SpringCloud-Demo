0abc80faab308a223231f99d2b4efd74ab5fa448
angrebobo <1172176452@qq.com>
2022-06-04 21:07:36 +0800

    Nacos

---
+++ eureka-server/src/main/java/cn/itcast/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.itcast.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: HuangSiBo
+ * @Description:
+ * @Data: Created in 15:58 2022/6/3
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
